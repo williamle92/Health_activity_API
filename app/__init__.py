53fccf3512a52309da9edd8fe6ab1f98a6a02dc9
williamle92 <william1992le@gmail.com>
2021-12-11 05:30:44 -0800

    added config, .env and connected to postgresql database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 @app.route("/")
 def index():
     return "Hello world"
+
+
